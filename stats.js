2af6dca9982b9dfb50b6b5118eb0b8583971b508
CallumBusuttil10 <166800341+CallumBusuttil10@users.noreply.github.com>
2024-04-19 14:22:18 +0000

    solution

--- stats.js
+++ stats.js
@@ -1,3 +1,4 @@
+const { count } = require("console");
 const fs = require("fs");
 
 const inputFile = "10000-most-common-passwords.csv";
@@ -10,10 +11,17 @@ if (fs.existsSync(outputFile)) { // check for existing output file
 
 const data = fs.readFileSync(inputFile, "utf-8");
 const lines = data.split(/\n/);
+let lengthOfPassword;
+let counter = {
+  1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0,
+  11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0
+};
 
 for (line of lines) {
-      
-      elements = line.split(delimiter);
-      console.log(elements)
-}
-
+      element = line.split(delimiter);
+      lengthOfPassword = element[1].length;
+      counter[lengthOfPassword]++;
+};
+  for (let item in counter){
+    fs.appendFileSync(outputFile, `Total Characters in the password: ${item} = ${counter[item]}\n`)
+  };
